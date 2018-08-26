5114568e281e1d8cffc2fbc9db0d7ff1aaf20fbc
WaYungGit <377209368@qq.com>
2018-08-26 13:05:57 +0800

    tool

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function EosjsInit()
 {
     var eosConfig = {
       chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-      httpEndpoint: 'http://openapi.eos.ren',
+      httpEndpoint: 'https://api1.eosasia.one',
       verbose: true
     }
 	
