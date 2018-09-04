c119bf18830c5143fa52f6db46cb79a813eec525
wayung <WaYung2018@gmail.com>
2018-09-04 23:19:41 +0800

    scatter test

--- js/main.js
+++ js/main.js
@@ -11,11 +11,54 @@ function Main(){
 
 	EosjsInit();
 
+	const network = {
+    blockchain:'eos',
+    protocol:'https',
+    host: $httpendpointid.val(),
+    port:443,
+    chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+	}
+
 	scatter.connect("ContractCommonTool").then(function(connected){
     // User does not have Scatter Desktop, Mobile or Classic installed.
     if(!connected) return false;
     window.scatter = null;
     console.log("scatter connect ok");
+
+    const requiredFields = { accounts:[network] };
+    scatter.getIdentity(requiredFields).then(() => {
+
+        // Always use the accounts you got back from Scatter. Never hardcode them even if you are prompting
+        // the user for their account name beforehand. They could still give you a different account.
+        const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+
+        // You can pass in any additional options you want into the eosjs reference.
+        const eosOptions = { expireInSeconds:60 };
+
+        // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
+        const eos = scatter.eos(network, Eos, eosOptions);
+
+        // ----------------------------
+        // Now that we have an identity,
+        // an EOSIO account, and a reference
+        // to an eosjs object we can send a transaction.
+        // ----------------------------
+
+
+        // Never assume the account's permission/authority. Always take it from the returned account.
+        const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
+
+        eos.transfer(account.name, 'helloworld', '1.0000 EOS', 'memo', transactionOptions).then(trx => {
+            // That's it!
+            console.log(`Transaction ID: ${trx.transaction_id}`);
+        }).catch(error => {
+            console.error(error);
+        });
+
+    }).catch(error => {
+        // The user rejected this request, or doesn't have the appropriate requirements.
+        console.error(error);
+    });
 	});
 
 	scatter.getIdentity(requiredFields).then(identity => {
