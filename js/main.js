6673d29d8e6a49ec379255a6878b6c7e3c5fe371
wayung <WaYung2018@gmail.com>
2018-09-05 23:35:08 +0800

    scatter test

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var g_curtable = '';
 var g_curaction = '';
 var g_eos = '';
 var g_abidata = '';
-function Main(){
+function Main() {
 	$("#tableid").hide();
 	$("#actionid").hide();
 	var $httpendpointid = $("#httpendpointid");
@@ -11,161 +11,139 @@ function Main(){
 
 	EosjsInit();
 
-	scatter.connect("TestPage").then(function(connected){
-        console.log('connected', connected);
-        var network = {blockchain:'eos', protocol:'https', host:'mainnet.eoscannon.io', port:443, chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'};
+	scatter.connect("TestPage").then(function (connected) {
+		console.log('connected', connected);
+		var network = { blockchain: 'eos', protocol: 'https', host: 'mainnet.eoscannon.io', port: 443, chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906' };
 
-	    var eos = scatter.eos(network, Eos);
+		var eos = scatter.eos(network, Eos);
 
-	    console.log('id before', scatter.identity);
-	    scatter.forgetIdentity().then(function(){
-	        scatter.getIdentity({accounts:[network]}).then(function(id){
-	            const account = id.accounts.find(function(x){ return x.blockchain === 'eos' });
-	            console.log('acc', account);
-	        })
-	    })
-    }).catch(function(x){
-        console.log('x', x);
-    });
+		console.log('id before', scatter.identity);
+		scatter.forgetIdentity().then(function () {
+			scatter.getIdentity({ accounts: [network] }).then(function (id) {
+				const account = id.accounts.find(function (x) { return x.blockchain === 'eos' });
+				console.log('acc', account);
+			})
+		})
+	}).catch(function (x) {
+		console.log('x', x);
+	});
 
-		
-	if(tp.isConnected() == true)
-	{
+
+	if (tp.isConnected() == true) {
 		tp.getWalletList('eos').then(data => {
-		var accountcnt = data["wallets"]["eos"].length;
-		var $accountlistid = $("#accountlistid");
-		$accountlistid.empty();
-		for(var i = 0; i <= accountcnt; i++)
-		{	
-			var accountname = data["wallets"]["eos"][i]["name"];
-			$accountlistid.append(new Option(accountname, accountname));
-			if(i == 0)
-			{
-				g_curwalletname = accountname;
+			var accountcnt = data["wallets"]["eos"].length;
+			var $accountlistid = $("#accountlistid");
+			$accountlistid.empty();
+			for (var i = 0; i <= accountcnt; i++) {
+				var accountname = data["wallets"]["eos"][i]["name"];
+				$accountlistid.append(new Option(accountname, accountname));
+				if (i == 0) {
+					g_curwalletname = accountname;
+				}
 			}
-		}
 		})
 	}
-	else{
+	else {
 		console.log("tp is not connected!");
 	}
 }
 
-function WalletChange(obj)
-{
+function WalletChange(obj) {
 	g_curwalletname = $(obj).val();
 }
 
-function TableChange(obj)
-{
+function TableChange(obj) {
 	g_curtable = $(obj).val();
 }
 
-function ActionChange(obj)
-{
+function ActionChange(obj) {
 	var type = $(obj).val();
 	ActionParamParse(type);
 }
 
-function ActionParamParse(action)
-{
+function ActionParamParse(action) {
 	var action_type;
 	var actioncnt = g_abidata["abi"]["actions"].length;
-	for(var i = 0; i < actioncnt; i++)
-	{
-		if(g_abidata["abi"]["actions"][i]["name"] == action)
-		{
+	for (var i = 0; i < actioncnt; i++) {
+		if (g_abidata["abi"]["actions"][i]["name"] == action) {
 			action_type = g_abidata["abi"]["actions"][i]["type"];
 			break;
 		}
 	}
-	
+
 	var fields;
 	var structcnt = g_abidata["abi"]["structs"].length;
-	for(var i = 0; i < structcnt; i++)
-	{
-		if(g_abidata["abi"]["structs"][i]["name"] == action_type)
-		{
+	for (var i = 0; i < structcnt; i++) {
+		if (g_abidata["abi"]["structs"][i]["name"] == action_type) {
 			fields = g_abidata["abi"]["structs"][i]["fields"];
 			break;
 		}
 	}
-	
+
 	$("#actionparamlistid").empty();
 	var fieldcnt = fields.length;
-	for(var i = 0; i < fieldcnt; i++)
-	{
-		var paramdiv = "<div class='row justify-content-center' style='margin:20px auto;'><div class='col-3'><label class='text-right'>"+fields[i]["name"]+"</label></div><div class='col-8'><input placeholder='"+fields[i]["type"]+"'></input></div></div>";
+	for (var i = 0; i < fieldcnt; i++) {
+		var paramdiv = "<div class='row justify-content-center' style='margin:20px auto;'><div class='col-3'><label class='text-right'>" + fields[i]["name"] + "</label></div><div class='col-8'><input placeholder='" + fields[i]["type"] + "'></input></div></div>";
 		$("#actionparamlistid").append(paramdiv);
 	}
 }
 
-function HttpEndPointChange(obj)
-{
+function HttpEndPointChange(obj) {
 	EosjsInit();
 }
 
-function OperateShow(type)
-{
-	if(type == 0)
-	{
+function OperateShow(type) {
+	if (type == 0) {
 		$("#tableid").hide();
 		$("#actionid").show();
 	}
-	else if(type == 1)
-	{
+	else if (type == 1) {
 		$("#tableid").show();
 		$("#actionid").hide();
 	}
 }
 
-function OperateChange(obj)
-{
+function OperateChange(obj) {
 	var type = $(obj).val();
 	OperateShow(type);
 }
 
-function OperateSubmit()
-{
+function OperateSubmit() {
 	var contract = $("#contractid").val();
 	var type = $("#operatetypeid").val();
-	if(type == 0)
-	{
+	if (type == 0) {
 		try {
 			var curaccount = g_curwalletname;
 			var action = $("#actionlistid").val();
 			var paramdata = '';
-			$("#actionparamlistid").find(".row").each(function(){
+			$("#actionparamlistid").find(".row").each(function () {
 				paramname = $(this).find("label").html();
 				paramval = $(this).find("input").val();
-				paramdata += '"'+paramname+'":"'+paramval+'",';
+				paramdata += '"' + paramname + '":"' + paramval + '",';
 			});
-			paramdata = paramdata.substring(0,paramdata.length-1)
-			
-			var actionstr = '{"actions":[{"account":"'+contract+'","name":"'+action+'","authorization":[{"actor":"'+curaccount+'","permission":"active"}],"data":{'+paramdata+'}}]}';
-			console.log("actionstr is "+actionstr);
+			paramdata = paramdata.substring(0, paramdata.length - 1)
+
+			var actionstr = '{"actions":[{"account":"' + contract + '","name":"' + action + '","authorization":[{"actor":"' + curaccount + '","permission":"active"}],"data":{' + paramdata + '}}]}';
+			console.log("actionstr is " + actionstr);
 			var params = JSON.parse(actionstr);
 			tp.pushEosAction(params).then(data => {
-			  var result = JSON.stringify(JSON.parse(JSON.stringify(data)), null, 2);
-			  $('#logid').html(result);
+				var result = JSON.stringify(JSON.parse(JSON.stringify(data)), null, 2);
+				$('#logid').html(result);
 			});
 		}
-		catch(e) {
-        $("#logid").html(e);
-		console.log(e);
+		catch (e) {
+			$("#logid").html(e);
+			console.log(e);
 		}
 	}
-	else if(type == 1)
-	{
+	else if (type == 1) {
 		var scope = $("#scopeid").val();
 		var table = $("#tablelistid").val();
-		g_eos.getTableRows(true, contract, scope, table, function(error, data){
-			if(error == null)
-			{
+		g_eos.getTableRows(true, contract, scope, table, function (error, data) {
+			if (error == null) {
 				$("#logid").html(JSON.stringify(data, null, 2));
 			}
-			else
-			{
+			else {
 				$("#logid").html(error);
 				console.log(error);
 			}
@@ -173,45 +151,40 @@ function OperateSubmit()
 	}
 }
 
-function pusheosshishicaiaddlink()
-{
-	if(tp.isConnected() == true)
-	{
+function pusheosshishicaiaddlink() {
+	if (tp.isConnected() == true) {
 		try {
 			var curaccount = g_curwalletname;
 			var upplayer = $("#upplayer").val();
-			var actionstr = '{"actions":[{"account":"eosshishicai","name":"addlink","authorization":[{"actor":"'+curaccount+'","permission":"active"}],"data":{"player":"'+curaccount+'","upplayer": "'+upplayer+'"}}]}';
+			var actionstr = '{"actions":[{"account":"eosshishicai","name":"addlink","authorization":[{"actor":"' + curaccount + '","permission":"active"}],"data":{"player":"' + curaccount + '","upplayer": "' + upplayer + '"}}]}';
 			var params = JSON.parse(actionstr);
 			tp.pushEosAction(params).then(data => {
-			  //var result = JSON.stringify(JSON.parse(JSON.stringify(data)), null, 2);
-			  //$('.consoleLog').html(result);
+				//var result = JSON.stringify(JSON.parse(JSON.stringify(data)), null, 2);
+				//$('.consoleLog').html(result);
 			});
-			
+
 		}
-		catch(e) {
-        //$('.consoleLog').html(e);
+		catch (e) {
+			//$('.consoleLog').html(e);
 		}
 	}
-	else
-	{
+	else {
 		console.log("tp is not connected!");
-		
+
 	}
 }
 
-function EosjsInit()
-{
-    var eosConfig = {
-      chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-      httpEndpoint: $("#httpendpointid").val(),
-      verbose: true
-    }
-	
+function EosjsInit() {
+	var eosConfig = {
+		chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+		httpEndpoint: $("#httpendpointid").val(),
+		verbose: true
+	}
+
 	g_eos = Eos(eosConfig);
 }
 
-function GetAbi()
-{
+function GetAbi() {
 	var $contractid = $("#contractid");
 	var $tablelistid = $("#tablelistid");
 	var $actionlistid = $("#actionlistid");
@@ -219,37 +192,33 @@ function GetAbi()
 	$actionlistid.empty();
 	$("#tableid").hide();
 	$("#actionid").hide();
-	g_eos.getAbi($contractid.val(), function(error, data) {
-	if(error == null)
-	{
-		g_abidata = data;
-		console.log(JSON.stringify(data, null, 2));
-		var tablecnt = data["abi"]["tables"].length;
-		for(var i = 0; i < tablecnt; i++)
-		{
-			var tablename = data["abi"]["tables"][i]["name"];
-			$tablelistid.append(new Option(tablename,tablename));
+	g_eos.getAbi($contractid.val(), function (error, data) {
+		if (error == null) {
+			g_abidata = data;
+			console.log(JSON.stringify(data, null, 2));
+			var tablecnt = data["abi"]["tables"].length;
+			for (var i = 0; i < tablecnt; i++) {
+				var tablename = data["abi"]["tables"][i]["name"];
+				$tablelistid.append(new Option(tablename, tablename));
+			}
+
+			var actioncnt = data["abi"]["actions"].length;
+			for (var i = 0; i < actioncnt; i++) {
+				var actionname = data["abi"]["actions"][i]["name"];
+				$actionlistid.append(new Option(actionname, actionname));
+			}
+
+			$("#operatetypeid").empty();
+			$("#operatetypeid").append(new Option("pushaction", 0));
+			$("#operatetypeid").append(new Option("gettable", 1));
+
+			OperateShow($("#operatetypeid").val());
+
+			ActionParamParse($actionlistid.val());
 		}
-		
-		var actioncnt = data["abi"]["actions"].length;
-		for(var i = 0; i < actioncnt; i++)
-		{
-			var actionname = data["abi"]["actions"][i]["name"];
-			$actionlistid.append(new Option(actionname,actionname));
+		else {
+			$("#logid").html(error);
+			console.log(error);
 		}
-		
-		$("#operatetypeid").empty();
-		$("#operatetypeid").append(new Option("pushaction",0));
-		$("#operatetypeid").append(new Option("gettable",1));
-		
-		OperateShow($("#operatetypeid").val());
-		
-		ActionParamParse($actionlistid.val());
-	}
-	else
-	{
-		$("#logid").html(error);
-		console.log(error);
-	}
 	})
 }
